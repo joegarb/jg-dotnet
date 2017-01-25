aa34af3fcb7e0a195443024f14c7fc73ad4393a7
Joe Garber <joegarb@users.noreply.github.com>
2017-01-25 15:37:07 -0500

    Fix demo assembly info

--- JG.Forms.Demo.AppBar/Properties/AssemblyInfo.cs
+++ JG.Forms.Demo.AppBar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stockbar")]
+[assembly: AssemblyTitle("JG.Forms.Demo.AppBar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stockbar")]
+[assembly: AssemblyProduct("JG.Forms.Demo.AppBar")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
